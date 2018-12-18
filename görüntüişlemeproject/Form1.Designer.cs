c253e88461017593e96239400a73774d8ea60dce
Nazlican <nazlicankahveci21@gmail.com>
2018-12-18 12:34:32 +0300

    ilk proje

---
+++ görüntüişlemeproject/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.camBox = new System.Windows.Forms.ComboBox();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.butonabaglan = new System.Windows.Forms.Button();
+            this.kaynakBox = new System.Windows.Forms.PictureBox();
+            this.islemBox = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.kaynakBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islemBox)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // camBox
+            // 
+            this.camBox.FormattingEnabled = true;
+            this.camBox.Location = new System.Drawing.Point(47, 27);
+            this.camBox.Name = "camBox";
+            this.camBox.Size = new System.Drawing.Size(246, 21);
+            this.camBox.TabIndex = 0;
+            this.camBox.SelectedIndexChanged += new System.EventHandler(this.camBox_SelectedIndexChanged);
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(422, 27);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(245, 21);
+            this.portBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(47, 392);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "RED";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(366, 392);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "GREEN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(689, 392);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "BLUE";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // butonabaglan
+            // 
+            this.butonabaglan.Location = new System.Drawing.Point(667, 25);
+            this.butonabaglan.Name = "butonabaglan";
+            this.butonabaglan.Size = new System.Drawing.Size(75, 23);
+            this.butonabaglan.TabIndex = 6;
+            this.butonabaglan.Text = "Bağlan";
+            this.butonabaglan.UseVisualStyleBackColor = true;
+            this.butonabaglan.Click += new System.EventHandler(this.butonabaglan_Click);
+            // 
+            // kaynakBox
+            // 
+            this.kaynakBox.Location = new System.Drawing.Point(47, 74);
+            this.kaynakBox.Name = "kaynakBox";
+            this.kaynakBox.Size = new System.Drawing.Size(320, 275);
+            this.kaynakBox.TabIndex = 7;
+            this.kaynakBox.TabStop = false;
+            // 
+            // islemBox
+            // 
+            this.islemBox.Location = new System.Drawing.Point(422, 74);
+            this.islemBox.Name = "islemBox";
+            this.islemBox.Size = new System.Drawing.Size(320, 275);
+            this.islemBox.TabIndex = 8;
+            this.islemBox.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(947, 25);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 440);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.islemBox);
+            this.Controls.Add(this.kaynakBox);
+            this.Controls.Add(this.butonabaglan);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.camBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kaynakBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islemBox)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox camBox;
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button butonabaglan;
+        private System.Windows.Forms.PictureBox kaynakBox;
+        private System.Windows.Forms.PictureBox islemBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ görüntüişlemeproject/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;  // arduino için input-output
+using AForge;
+using AForge.Video;    // new frame için
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Vision;
+using AForge.Vision.Motion;
+using AForge.Math.Geometry;
+using Point = System.Drawing.Point;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection tumkameralar;  //bilgisayarda ki tüm kameralar dizi olur
+        private VideoCaptureDevice kamera;            //kullanılan kamera
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kamera = new VideoCaptureDevice(tumkameralar[camBox.SelectedIndex].MonikerString); //tüm kameralar açılıyor(liste olarak) ve kullanılan kemara seçilir.
+
+            kamera.NewFrame += new NewFrameEventHandler(kamera_NewFrame); //ekranda çıkan tüm yeni karelerin hepsi işlenir(gerçek zamanlı), çıkan karede işlem yapabiliriz.
+
+            kamera.DesiredFrameRate = 20;
+            kamera.DesiredFrameSize = new Size(350, 240);
+            kamera.Start();
+        }
+        void kamera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone(); //2 görüntü oluşturdum.İkiside aynı görüntü.
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone(); //image1 radio button da kullandım.
+            kaynakBox.Image = image; //ilk görüntüyü kaynak box a atadım.
+
+
+            if (radioButton1.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();  //filtre oluşturdum.
+                filter.CenterColor = new RGB(Color.FromArgb(215, 25, 25));
+                filter.Radius = 100; //radius çevresi içerisi boyunca kırmızı algılanacak
+                filter.ApplyInPlace(image1);   //image1 görüntü üzerine filtre uyguladım. Arka plan siyah oldu
+                nesnebul(image1);
+            }
+            if (radioButton2.Checked)
+            {
+                EuclideanColorFiltering filtre = new EuclideanColorFiltering();
+
+                filtre.CenterColor = new RGB(Color.FromArgb(30, 215, 30));
+                filtre.Radius = 100;
+
+                filtre.ApplyInPlace(image1);
+
+                nesnebul(image1); //görüntünün işlenmiş halini nesne bul a gönderdim.
+
+            }
+            if (radioButton3.Checked)
+            {
+                EuclideanColorFiltering filtre = new EuclideanColorFiltering();
+
+                filtre.CenterColor = new RGB(Color.FromArgb(30, 144, 255));
+                filtre.Radius = 100;
+
+                filtre.ApplyInPlace(image1);
+
+                nesnebul(image1);
+            }
+        }
+        public void nesnebul(Bitmap image1) //işlenmiş görüntü
+        {
+
+            BlobCounter blobCounter = new BlobCounter();  // obje say. Görüntü işlemede blobları kullanarak şekil tespiti sağlanabilir.
+            blobCounter.MinWidth = 5;  //ne kadar küçük olursa hassasiyet artar. çerçevenin içine alır
+            blobCounter.MinHeight = 5;
+            blobCounter.FilterBlobs = true; //filtre uygula
+            blobCounter.ProcessImage(image1);  //Blob bir resmin içinde bulunan görüntü parçalarıdır.Blob counter'ı image1 yani işlenmiş görüntü üzerine uyguladım.tekrar işledim.
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();  //oluşan her bir kareden bilgi alıp,diziye attım.
+
+
+            islemBox.Image = image1;  //işlenmiş son görüntüyü işlem box a attım
+
+
+            foreach (Rectangle recs in rects)
+            {
+
+                if (rects.Length > 0) //karelerden bilgiler alınmışsa (eğer)
+                {
+                    Rectangle objectRect = rects[0]; //ilk yakalanan kare obje oldu. 
+
+                    Graphics g = kaynakBox.CreateGraphics(); //Kontrol için bir Grafik nesnesi oluşturma
+
+                    using (Pen pen = new Pen(Color.FromArgb(250, 0, 0), 2)) //kaynakbox ta objeyi çizen diktörtgenin rengi ve kalınlığı 
+                    {
+                        g.DrawRectangle(pen, objectRect);
+                    }
+
+                    int objectX = objectRect.X + (objectRect.Width / 2);  //yeniden boyutlandırma için
+                    int objectY = objectRect.Y + (objectRect.Height / 2);
+
+                    g.Dispose(); //kontrolde kalması için  yaptım
+
+
+
+
+
+                    if (objectX < 106 && objectY < 91) //320:275 lik box boyutum bu nedenle 9 kutu için yaklaşık değer verdim
+                    {
+
+                        serialPort1.Write("1");    
+
+                    }
+                    else if ((objectX > 106 && objectX < 212) && (objectY < 91))
+                    {
+
+                        serialPort1.Write("2");
+                    }
+                    else if ((objectX > 212 && objectX < 320) && (objectY < 91))
+                    {
+
+                        serialPort1.Write("3");
+                    }
+                    else if ((objectX < 106) && (objectY > 91 && objectY < 182))
+                    {
+
+                        serialPort1.Write("4");
+                    }
+                    else if ((objectX > 106 && objectX < 212) && (objectY > 91 && objectY < 182))
+                    {
+
+                        serialPort1.Write("5");
+                    }
+                    else if ((objectX > 212 && objectX < 320) && (objectY > 91 && objectY < 182))
+                    {
+
+                        serialPort1.Write("6");
+                    }
+                    else if ((objectX < 106) && (objectY > 182 && objectY < 275))
+                    {
+
+                        serialPort1.Write("7");
+                    }
+                    else if ((objectX > 106 && objectX < 212) && (objectY > 182 && objectY < 275))
+                    {
+
+                        serialPort1.Write("8");
+                    }
+                    else if ((objectX > 212 && objectX < 320) && (objectY > 182 && objectY < 275))
+                    {
+
+                        serialPort1.Write("9");
+                    }
+
+
+                }
+            }
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void camBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butonabaglan_Click(object sender, EventArgs e)
+        {
+            //arduino için:
+            serialPort1.PortName = portBox.SelectedItem.ToString(); //port box ta seçilen string benim port'umdur.
+            serialPort1.BaudRate = 9600; //haberleşme hızı
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                toolStripLabel1.Text = portBox.SelectedItem.ToString() + "portuna bağlandı"; //string buraya geldi.(serial port name)
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            portBox.DataSource = SerialPort.GetPortNames();  //arduino'mdaki tüm port boxlar listelendi
+            int sayi = portBox.Items.Count;
+
+            if (sayi == 0)
+            {
+                toolStripLabel1.Text = "Port Bulunamadı.Kontrol et!";
+                portBox.Enabled = false;
+                butonabaglan.Enabled = false; //aktif olmadı
+            }
+
+            else
+            {
+
+                toolStripLabel1.Text = sayi + "Tane Port var";
+            }
+
+
+            tumkameralar = new FilterInfoCollection(FilterCategory.VideoInputDevice); //bilgisayarımdaki tüm kameralar toplnıp değişkene atandı
+
+            foreach (FilterInfo VideoCaptureDevice in tumkameralar)
+            {
+
+                camBox.Items.Add(VideoCaptureDevice.Name); //liste halinde gösterildi
+
+            }
+
+            camBox.SelectedIndex = 0; //ilk ekran açılınca kutu boş gözüküyor , biz seçiyoruz, bunu yazmazsak.
+
+        }
+
+    }
+}
